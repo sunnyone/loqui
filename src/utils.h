67ffce4696c35bf1afbd71b584d14cd75c584c27
sunnyone41 <>
2003-02-22 08:29:00 +0000

    fixed DEBUG_TIMER_STOP

--- src/utils.h
+++ src/utils.h
@@ -33,7 +33,7 @@
    g_timer_start(timer); \
 }
 #define DEBUG_TIMER_STOP(timer, name) { \
-   timer = g_timer_stop(timer); \
+   g_timer_stop(timer); \
    g_print("elapsed(%s): %.5f\n", name, g_timer_elapsed(timer, NULL)); \
    g_timer_destroy(timer); \
 }
