3f5c61593d78bf3646d705b2b723881b748be458
sunnyone41 <>
2004-03-19 14:24:48 +0000

    Added LoquiUser.

---
+++ src/loqui_user.c
@@ -0,0 +1,358 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Loqui -- Chat client for Gtk2 <http://loqui.good-day.net/>
+ * Copyright (C) 2004 Yoichi Imai <yoichi@silver-forest.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+#include "config.h"
+
+#include "loqui_user.h"
+#include "intl.h"
+
+#include <string.h>
+
+enum {
+        LAST_SIGNAL
+};
+
+enum {
+	PROP_0,
+	PROP_NICK,
+	PROP_USERNAME,
+	PROP_HOSTNAME,
+	PROP_REALNAME,
+	PROP_SERVERNAME,
+	PROP_AWAY,
+	PROP_AWAY_MESSAGE,
+	PROP_IDLE_TIME,
+        LAST_PROP
+};
+
+struct _LoquiUserPrivate
+{
+};
+
+static GObjectClass *parent_class = NULL;
+
+#define AWAY_TYPE_ARRAY_DEFAULT_SIZE 10
+
+/* static guint loqui_user_signals[LAST_SIGNAL] = { 0 }; */
+
+static void loqui_user_class_init(LoquiUserClass *klass);
+static void loqui_user_init(LoquiUser *user);
+static void loqui_user_finalize(GObject *object);
+static void loqui_user_dispose(GObject *object);
+
+static void loqui_user_get_property(GObject *object, guint param_id, GValue *value, GParamSpec *pspec);
+static void loqui_user_set_property(GObject *object, guint param_id, const GValue *value, GParamSpec *pspec);
+
+GType
+loqui_user_get_type(void)
+{
+	static GType type = 0;
+	if (type == 0) {
+		static const GTypeInfo our_info =
+			{
+				sizeof(LoquiUserClass),
+				NULL,           /* base_init */
+				NULL,           /* base_finalize */
+				(GClassInitFunc) loqui_user_class_init,
+				NULL,           /* class_finalize */
+				NULL,           /* class_data */
+				sizeof(LoquiUser),
+				0,              /* n_preallocs */
+				(GInstanceInitFunc) loqui_user_init
+			};
+		
+		type = g_type_register_static(G_TYPE_OBJECT,
+					      "LoquiUser",
+					      &our_info,
+					      0);
+	}
+	
+	return type;
+}
+static void 
+loqui_user_finalize(GObject *object)
+{
+	LoquiUser *user;
+
+        g_return_if_fail(object != NULL);
+        g_return_if_fail(LOQUI_IS_USER(object));
+
+        user = LOQUI_USER(object);
+
+        if (G_OBJECT_CLASS(parent_class)->finalize)
+                (* G_OBJECT_CLASS(parent_class)->finalize)(object);
+
+	g_free(user->priv);
+}
+static void 
+loqui_user_dispose(GObject *object)
+{
+	LoquiUser *user;
+
+        g_return_if_fail(object != NULL);
+        g_return_if_fail(LOQUI_IS_USER(object));
+
+        user = LOQUI_USER(object);
+
+        if (G_OBJECT_CLASS(parent_class)->dispose)
+                (* G_OBJECT_CLASS(parent_class)->dispose)(object);
+}
+static void
+loqui_user_get_property(GObject *object, guint param_id, GValue *value, GParamSpec *pspec)
+{
+        LoquiUser *user;        
+
+        user = LOQUI_USER(object);
+
+        switch (param_id) {
+	case PROP_NICK:
+		g_value_set_string(value, user->nick);
+		break;
+	case PROP_USERNAME:
+		g_value_set_string(value, user->username);
+		break;
+	case PROP_HOSTNAME:
+		g_value_set_string(value, user->hostname);
+		break;
+	case PROP_REALNAME:
+		g_value_set_string(value, user->realname);
+		break;
+	case PROP_SERVERNAME:
+		g_value_set_string(value, user->servername);
+		break;
+	case PROP_AWAY_MESSAGE:
+		g_value_set_string(value, user->away_message);
+		break;
+	case PROP_AWAY:
+		g_value_set_int(value, user->away);
+		break;
+	case PROP_IDLE_TIME:
+		g_value_set_int(value, user->idle_time);
+		break;
+        default:
+                G_OBJECT_WARN_INVALID_PROPERTY_ID(object, param_id, pspec);
+                break;
+        }
+}
+static void
+loqui_user_set_property(GObject *object, guint param_id, const GValue *value, GParamSpec *pspec)
+{
+        LoquiUser *user;        
+
+        user = LOQUI_USER(object);
+
+        switch (param_id) {
+	case PROP_NICK:
+		loqui_user_set_nick(user, g_value_get_string(value));
+		break;
+	case PROP_USERNAME:
+		loqui_user_set_username(user, g_value_get_string(value));
+		break;
+	case PROP_HOSTNAME:
+		loqui_user_set_hostname(user, g_value_get_string(value));
+		break;
+	case PROP_REALNAME:
+		loqui_user_set_realname(user, g_value_get_string(value));
+		break;
+	case PROP_SERVERNAME:
+		loqui_user_set_servername(user, g_value_get_string(value));
+		break;
+	case PROP_AWAY_MESSAGE:
+		loqui_user_set_away_message(user, g_value_get_string(value));
+		break;
+	case PROP_AWAY:
+		loqui_user_set_away(user, g_value_get_int(value));
+		break;
+	case PROP_IDLE_TIME:
+		loqui_user_set_idle_time(user, g_value_get_int(value));
+		break;
+        default:
+                G_OBJECT_WARN_INVALID_PROPERTY_ID(object, param_id, pspec);
+                break;
+        }
+}
+
+static void
+loqui_user_class_init(LoquiUserClass *klass)
+{
+        GObjectClass *object_class = G_OBJECT_CLASS(klass);
+	LoquiAwayType atype;
+	gpointer null_ptr = NULL;
+
+        parent_class = g_type_class_peek_parent(klass);
+        
+        object_class->finalize = loqui_user_finalize;
+        object_class->dispose = loqui_user_dispose;
+        object_class->get_property = loqui_user_get_property;
+        object_class->set_property = loqui_user_set_property;
+
+	g_object_class_install_property(object_class,
+					PROP_NICK,
+					g_param_spec_string("nick",
+							    _("Nick"),
+							    _("Nick name"),
+							    NULL, G_PARAM_READWRITE));
+	g_object_class_install_property(object_class,
+					PROP_USERNAME,
+					g_param_spec_string("username",
+							    _("Username"),
+							    _("Username"),
+							    NULL, G_PARAM_READWRITE));
+	g_object_class_install_property(object_class,
+					PROP_HOSTNAME,
+					g_param_spec_string("hostname",
+							    _("Hostname"),
+							    _("Hostname"),
+							    NULL, G_PARAM_READWRITE));
+	g_object_class_install_property(object_class,
+					PROP_REALNAME,
+					g_param_spec_string("realname",
+							    _("Realname"),
+							    _("Realname"),
+							    NULL, G_PARAM_READWRITE));
+	g_object_class_install_property(object_class,
+					PROP_SERVERNAME,
+					g_param_spec_string("servername",
+							    _("Server name"),
+							    _("Server name"),
+							    NULL, G_PARAM_READWRITE));
+	g_object_class_install_property(object_class,
+					PROP_AWAY_MESSAGE,
+					g_param_spec_string("away_message",
+							    _("Away message"),
+							    _("Away message"),
+							    NULL, G_PARAM_READWRITE));
+	g_object_class_install_property(object_class,
+					PROP_AWAY,
+					g_param_spec_int("away",
+							 _("Away type"),
+							 _("Away type (LoquiAwayType)"),
+							 1, G_MAXINT,
+							 LOQUI_AWAY_TYPE_ONLINE, G_PARAM_READWRITE));
+	g_object_class_install_property(object_class,
+					PROP_IDLE_TIME,
+					g_param_spec_int("idle_time",
+							 _("Idle time"),
+							 _("Idle time (from epoch seconds)"),
+							 G_MININT, G_MAXINT,
+							 0, G_PARAM_READWRITE));
+
+	klass->away_type_array = g_ptr_array_sized_new(AWAY_TYPE_ARRAY_DEFAULT_SIZE);
+	g_ptr_array_add(klass->away_type_array, null_ptr);
+
+	atype = loqui_user_class_install_away_type(klass, LOQUI_BASIC_AWAY_TYPE_ONLINE, "online", _("Online"));
+	g_assert(atype == LOQUI_AWAY_TYPE_ONLINE);
+
+	atype = loqui_user_class_install_away_type(klass, LOQUI_BASIC_AWAY_TYPE_OFFLINE, "offline", _("Offline"));
+	g_assert(atype == LOQUI_AWAY_TYPE_OFFLINE);
+
+	atype = loqui_user_class_install_away_type(klass, LOQUI_BASIC_AWAY_TYPE_AWAY, "away", _("Away"));
+	g_assert(atype == LOQUI_AWAY_TYPE_AWAY);
+}
+
+static void 
+loqui_user_init(LoquiUser *user)
+{
+	LoquiUserPrivate *priv;
+
+	priv = g_new0(LoquiUserPrivate, 1);
+
+	user->priv = priv;
+}
+LoquiUser*
+loqui_user_new(void)
+{
+        LoquiUser *user;
+	LoquiUserPrivate *priv;
+
+	user = g_object_new(loqui_user_get_type(), NULL);
+	
+        priv = user->priv;
+
+        return user;
+}
+
+/**
+   loqui_user_class_install_away_type:
+   @name: away name like "protocol-busy". must be unique.
+   @nick: away type nick, like "Online".
+*/
+LoquiAwayType
+loqui_user_class_install_away_type(LoquiUserClass *user_class,
+				   LoquiBasicAwayType basic_away_type,
+				   const gchar *name,
+				   const gchar *nick)
+{
+	LoquiAwayInfo *info;
+	guint i;
+
+        g_return_val_if_fail(user_class != NULL, LOQUI_AWAY_TYPE_INVALID);
+        g_return_val_if_fail(LOQUI_IS_USER_CLASS(user_class), LOQUI_AWAY_TYPE_INVALID);
+	g_return_val_if_fail(name != NULL, LOQUI_AWAY_TYPE_INVALID);
+	g_return_val_if_fail(nick != NULL, LOQUI_AWAY_TYPE_INVALID);
+
+	/* if zero, uninitialized */
+	g_assert(user_class->away_type_array->len > 0);
+	
+	for (i = 0; i < user_class->away_type_array->len; i++) {
+		info = g_ptr_array_index(user_class->away_type_array, i);
+		if(!info)
+			continue;
+
+		if (strcmp(info->name, name) == 0)
+			return LOQUI_AWAY_TYPE_INVALID;
+	}
+
+	info = g_new0(LoquiAwayInfo, 1);
+	
+	info->basic_away_type = basic_away_type;
+	info->name = g_strdup(name);
+	info->nick = g_strdup(nick);
+
+	g_ptr_array_add(user_class->away_type_array, info);
+	
+	info->away_type = user_class->away_type_array->len - 1;
+
+	return info->away_type;
+}
+
+LoquiAwayInfo *
+loqui_user_class_away_type_get_info(LoquiUserClass *user_class, LoquiAwayType away_type)
+{
+	LoquiAwayInfo *info;
+	
+        g_return_val_if_fail(user_class != NULL, NULL);
+        g_return_val_if_fail(LOQUI_IS_USER_CLASS(user_class), NULL);
+	
+	if (away_type < 0 || away_type >= user_class->away_type_array->len)
+		return LOQUI_AWAY_TYPE_INVALID;
+
+	info = g_ptr_array_index(user_class->away_type_array, away_type);
+	
+	return info;
+}
+
+LOQUI_USER_ACCESSOR_GENERIC(gint, idle_time);
+LOQUI_USER_ACCESSOR_GENERIC(LoquiAwayType, away);
+LOQUI_USER_ACCESSOR_STRING(nick);
+LOQUI_USER_ACCESSOR_STRING(username);
+LOQUI_USER_ACCESSOR_STRING(hostname);
+LOQUI_USER_ACCESSOR_STRING(realname);
+LOQUI_USER_ACCESSOR_STRING(servername);
+LOQUI_USER_ACCESSOR_STRING(away_message);
---
+++ src/loqui_user.h
@@ -0,0 +1,125 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Loqui -- Chat client for Gtk2 <http://loqui.good-day.net/>
+ * Copyright (C) 2004 Yoichi Imai <yoichi@silver-forest.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+#ifndef __LOQUI_USER_H__
+#define __LOQUI_USER_H__
+
+#include <glib-object.h>
+#include "gobject_utils.h"
+
+G_BEGIN_DECLS
+
+#define LOQUI_TYPE_USER                 (loqui_user_get_type ())
+#define LOQUI_USER(obj)                 (G_TYPE_CHECK_INSTANCE_CAST ((obj), LOQUI_TYPE_USER, LoquiUser))
+#define LOQUI_USER_CLASS(klass)         (G_TYPE_CHECK_CLASS_CAST ((klass), LOQUI_TYPE_USER, LoquiUserClass))
+#define LOQUI_IS_USER(obj)              (G_TYPE_CHECK_INSTANCE_TYPE ((obj), LOQUI_TYPE_USER))
+#define LOQUI_IS_USER_CLASS(klass)      (G_TYPE_CHECK_CLASS_TYPE ((klass), LOQUI_TYPE_USER))
+#define LOQUI_USER_GET_CLASS(obj)       (G_TYPE_INSTANCE_GET_CLASS ((obj), LOQUI_TYPE_USER, LoquiUserClass))
+
+typedef struct _LoquiUser            LoquiUser;
+typedef struct _LoquiUserClass       LoquiUserClass;
+
+typedef struct _LoquiUserPrivate     LoquiUserPrivate;
+
+typedef enum {
+	LOQUI_BASIC_AWAY_TYPE_INVALID = 0,
+	LOQUI_BASIC_AWAY_TYPE_ONLINE,
+	LOQUI_BASIC_AWAY_TYPE_OFFLINE,
+	LOQUI_BASIC_AWAY_TYPE_AWAY,
+	LOQUI_BASIC_AWAY_TYPE_BUSY,
+} LoquiBasicAwayType;
+
+typedef enum {
+	LOQUI_AWAY_TYPE_INVALID = 0,
+	LOQUI_AWAY_TYPE_ONLINE,
+	LOQUI_AWAY_TYPE_OFFLINE,
+	LOQUI_AWAY_TYPE_AWAY
+} LoquiAwayType;
+/* AwayType is registerble, 
+   for example, IRC has away only, but IM protocol has many away type */
+
+typedef struct _LoquiAwayInfo {
+	LoquiAwayType away_type;
+	LoquiBasicAwayType basic_away_type;
+	gchar *name;
+	gchar *nick;
+} LoquiAwayInfo;
+
+struct _LoquiUser
+{
+        GObject parent;
+        
+	gchar *nick;
+
+	/* mailaddress == username@hostname */
+	gchar *username;
+	gchar *hostname;
+
+	gchar *realname;
+
+	gchar *servername;
+
+	LoquiAwayType away;
+	gchar *away_message;
+	
+	gint idle_time; /* time_t ? */
+
+        LoquiUserPrivate *priv;
+};
+
+struct _LoquiUserClass
+{
+        GObjectClass parent_class;
+
+	GPtrArray *away_type_array;
+};
+
+#define LOQUI_USER_ACCESSOR_STRING(attr_name) \
+  ATTR_ACCESSOR_POINTER(g_strdup, g_free, const gchar *, G_CONST_RETURN gchar *, LoquiUser, loqui_user, attr_name)
+#define LOQUI_USER_ACCESSOR_STRING_PROTOTYPE(attr_name) \
+  ATTR_ACCESSOR_POINTER_PROTOTYPE(const gchar *, G_CONST_RETURN gchar *, LoquiUser, loqui_user, attr_name)
+
+#define LOQUI_USER_ACCESSOR_GENERIC(type, attr_name) \
+  ATTR_ACCESSOR_GENERIC(type, 0, LoquiUser, loqui_user, attr_name)
+#define LOQUI_USER_ACCESSOR_GENERIC_PROTOTYPE(type, attr_name) \
+  ATTR_ACCESSOR_GENERIC_PROTOTYPE(type, LoquiUser, loqui_user, attr_name)
+
+GType loqui_user_get_type(void) G_GNUC_CONST;
+
+LoquiUser* loqui_user_new(void);
+
+LoquiAwayType loqui_user_class_install_away_type(LoquiUserClass *user_class,
+						 LoquiBasicAwayType basic_away_type,
+						 const gchar *name,
+						 const gchar *nick);
+
+LoquiAwayInfo* loqui_user_class_away_type_get_info(LoquiUserClass *user_class, LoquiAwayType away_type);
+
+LOQUI_USER_ACCESSOR_GENERIC_PROTOTYPE(gint, idle_time);
+LOQUI_USER_ACCESSOR_GENERIC_PROTOTYPE(LoquiAwayType, away);
+LOQUI_USER_ACCESSOR_STRING_PROTOTYPE(nick);
+LOQUI_USER_ACCESSOR_STRING_PROTOTYPE(username);
+LOQUI_USER_ACCESSOR_STRING_PROTOTYPE(hostname);
+LOQUI_USER_ACCESSOR_STRING_PROTOTYPE(realname);
+LOQUI_USER_ACCESSOR_STRING_PROTOTYPE(servername);
+LOQUI_USER_ACCESSOR_STRING_PROTOTYPE(away_message);
+
+G_END_DECLS
+
+#endif /* __LOQUI_USER_H__ */
