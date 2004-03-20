c40fe412a5e0e31430a7b48b88fc88825acd04d7
sunnyone41 <>
2004-03-20 08:30:48 +0000

    Added LoquiMember class.

---
+++ src/loqui_member.c
@@ -0,0 +1,250 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * libloqui -- Chat/IM library with GNet/GObject <http://loqui.good-day.net/>
+ * Copyright (C) 2004 Yoichi Imai <yoichi@silver-forest.com>
+ *
+ * This Library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with the Gnome Library; see the file COPYING.LIB.  If not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+#include "config.h"
+#include "intl.h"
+
+#include "loqui_member.h"
+
+enum {
+        LAST_SIGNAL
+};
+
+enum {
+	PROP_0,
+	PROP_USER,
+	PROP_POWER,
+        LAST_PROP
+};
+
+struct _LoquiMemberPrivate
+{
+};
+
+static GObjectClass *parent_class = NULL;
+
+/* static guint loqui_member_signals[LAST_SIGNAL] = { 0 }; */
+
+static void loqui_member_class_init(LoquiMemberClass *klass);
+static void loqui_member_init(LoquiMember *member);
+static void loqui_member_finalize(GObject *object);
+static void loqui_member_dispose(GObject *object);
+
+static void loqui_member_get_property(GObject *object, guint param_id, GValue *value, GParamSpec *pspec);
+static void loqui_member_set_property(GObject *object, guint param_id, const GValue *value, GParamSpec *pspec);
+
+GType
+loqui_member_get_type(void)
+{
+	static GType type = 0;
+	if (type == 0) {
+		static const GTypeInfo our_info =
+			{
+				sizeof(LoquiMemberClass),
+				NULL,           /* base_init */
+				NULL,           /* base_finalize */
+				(GClassInitFunc) loqui_member_class_init,
+				NULL,           /* class_finalize */
+				NULL,           /* class_data */
+				sizeof(LoquiMember),
+				0,              /* n_preallocs */
+				(GInstanceInitFunc) loqui_member_init
+			};
+		
+		type = g_type_register_static(G_TYPE_OBJECT,
+					      "LoquiMember",
+					      &our_info,
+					      0);
+	}
+	
+	return type;
+}
+static void 
+loqui_member_finalize(GObject *object)
+{
+	LoquiMember *member;
+
+        g_return_if_fail(object != NULL);
+        g_return_if_fail(LOQUI_IS_MEMBER(object));
+
+        member = LOQUI_MEMBER(object);
+
+        if (G_OBJECT_CLASS(parent_class)->finalize)
+                (* G_OBJECT_CLASS(parent_class)->finalize)(object);
+
+	g_free(member->priv);
+}
+static void 
+loqui_member_dispose(GObject *object)
+{
+	LoquiMember *member;
+
+        g_return_if_fail(object != NULL);
+        g_return_if_fail(LOQUI_IS_MEMBER(object));
+
+        member = LOQUI_MEMBER(object);
+
+        if (G_OBJECT_CLASS(parent_class)->dispose)
+                (* G_OBJECT_CLASS(parent_class)->dispose)(object);
+}
+static void
+loqui_member_get_property(GObject *object, guint param_id, GValue *value, GParamSpec *pspec)
+{
+        LoquiMember *member;        
+
+        member = LOQUI_MEMBER(object);
+
+        switch (param_id) {
+	case PROP_USER:
+		g_value_set_object(value, member->user);
+		break;
+	case PROP_POWER:
+		g_value_set_enum(value, member->power);
+		break;
+        default:
+                G_OBJECT_WARN_INVALID_PROPERTY_ID(object, param_id, pspec);
+                break;
+        }
+}
+static void
+loqui_member_set_property(GObject *object, guint param_id, const GValue *value, GParamSpec *pspec)
+{
+        LoquiMember *member;        
+
+        member = LOQUI_MEMBER(object);
+
+        switch (param_id) {
+	case PROP_USER:
+		loqui_member_set_user(member, g_value_get_object(value));
+		break;
+	case PROP_POWER:
+		loqui_member_set_power(member, g_value_get_enum(value));
+		break;
+        default:
+                G_OBJECT_WARN_INVALID_PROPERTY_ID(object, param_id, pspec);
+                break;
+        }
+}
+
+GType
+loqui_member_power_type_get_type(void)
+{
+	static GType etype = 0;
+	if (etype == 0) {
+		static const GEnumValue values[] = {
+			{LOQUI_MEMBER_POWER_UNDETERMINED, "LOQUI_MEMBER_POWER_UNDETERMINED", "Undetermined" },
+			{LOQUI_MEMBER_POWER_NONE, "LOQUI_MEMBER_POWER_NONE", "None"},
+			{LOQUI_MEMBER_POWER_VOICE, "LOQUI_MEMBER_POWER_VOICE", "Voice"},
+			{LOQUI_MEMBER_POWER_OP, "LOQUI_MEMBER_POWER_OP", "Op"},
+			{0, NULL, NULL}
+		};
+		etype = g_enum_register_static("LoquiMemberPowerType", values);
+	}
+	return etype;
+}
+static void
+loqui_member_class_init(LoquiMemberClass *klass)
+{
+        GObjectClass *object_class = G_OBJECT_CLASS(klass);
+
+        parent_class = g_type_class_peek_parent(klass);
+        
+        object_class->finalize = loqui_member_finalize;
+        object_class->dispose = loqui_member_dispose;
+        object_class->get_property = loqui_member_get_property;
+        object_class->set_property = loqui_member_set_property;
+
+	g_object_class_install_property(object_class,
+					PROP_USER,
+					g_param_spec_object("user",
+							    _("User"),
+							    _("User"),
+							    LOQUI_TYPE_USER, G_PARAM_READWRITE));
+	g_object_class_install_property(object_class,
+					PROP_POWER,
+					g_param_spec_enum("power",
+							  _("Power"),
+							  _("Power"),
+							  LOQUI_TYPE_MEMBER_POWER_TYPE,
+							  LOQUI_MEMBER_POWER_UNDETERMINED,
+							  G_PARAM_READWRITE));
+}
+static void 
+loqui_member_init(LoquiMember *member)
+{
+	LoquiMemberPrivate *priv;
+
+	priv = g_new0(LoquiMemberPrivate, 1);
+
+	member->priv = priv;
+}
+LoquiMember*
+loqui_member_new(void)
+{
+        LoquiMember *member;
+	LoquiMemberPrivate *priv;
+
+	member = g_object_new(loqui_member_get_type(), NULL);
+	
+        priv = member->priv;
+
+        return member;
+}
+void
+loqui_member_set_user(LoquiMember *member, LoquiUser *user)
+{
+        g_return_if_fail(member != NULL);
+        g_return_if_fail(LOQUI_IS_MEMBER(member));
+
+	G_OBJECT_UNREF_UNLESS_NULL(member->user);
+
+	if (user) {
+		g_object_ref(user);
+		member->user = user;
+	}
+	g_object_notify(G_OBJECT(member), "user");
+}
+LoquiUser*
+loqui_member_get_user(LoquiMember *member)
+{
+        g_return_val_if_fail(member != NULL, NULL);
+        g_return_val_if_fail(LOQUI_IS_MEMBER(member), NULL);
+
+	return member->user;
+}
+void
+loqui_member_set_power(LoquiMember *member, LoquiMemberPowerType power)
+{
+        g_return_if_fail(member != NULL);
+        g_return_if_fail(LOQUI_IS_MEMBER(member));
+	
+	member->power = power;
+
+	g_object_notify(G_OBJECT(member), "power");
+}
+
+LoquiMemberPowerType
+loqui_member_get_power(LoquiMember *member)
+{
+        g_return_val_if_fail(member != NULL, 0);
+        g_return_val_if_fail(LOQUI_IS_MEMBER(member), 0);
+
+	return member->power;
+}
---
+++ src/loqui_member.h
@@ -0,0 +1,79 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * libloqui -- Chat/IM library with GNet/GObject <http://loqui.good-day.net/>
+ * Copyright (C) 2004 Yoichi Imai <yoichi@silver-forest.com>
+ *
+ * This Library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with the Gnome Library; see the file COPYING.LIB.  If not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+#ifndef __LOQUI_MEMBER_H__
+#define __LOQUI_MEMBER_H__
+
+#include <glib-object.h>
+#include "loqui_user.h"
+
+G_BEGIN_DECLS
+
+#define LOQUI_TYPE_MEMBER                 (loqui_member_get_type ())
+#define LOQUI_MEMBER(obj)                 (G_TYPE_CHECK_INSTANCE_CAST ((obj), LOQUI_TYPE_MEMBER, LoquiMember))
+#define LOQUI_MEMBER_CLASS(klass)         (G_TYPE_CHECK_CLASS_CAST ((klass), LOQUI_TYPE_MEMBER, LoquiMemberClass))
+#define LOQUI_IS_MEMBER(obj)              (G_TYPE_CHECK_INSTANCE_TYPE ((obj), LOQUI_TYPE_MEMBER))
+#define LOQUI_IS_MEMBER_CLASS(klass)      (G_TYPE_CHECK_CLASS_TYPE ((klass), LOQUI_TYPE_MEMBER))
+#define LOQUI_MEMBER_GET_CLASS(obj)       (G_TYPE_INSTANCE_GET_CLASS ((obj), LOQUI_TYPE_MEMBER, LoquiMemberClass))
+
+#define LOQUI_TYPE_MEMBER_POWER_TYPE      (loqui_member_power_type_get_type())
+
+typedef struct _LoquiMember            LoquiMember;
+typedef struct _LoquiMemberClass       LoquiMemberClass;
+
+typedef struct _LoquiMemberPrivate     LoquiMemberPrivate;
+
+typedef enum {
+	LOQUI_MEMBER_POWER_UNDETERMINED,
+	LOQUI_MEMBER_POWER_NONE,
+	LOQUI_MEMBER_POWER_VOICE,
+	LOQUI_MEMBER_POWER_OP
+} LoquiMemberPowerType;
+
+struct _LoquiMember
+{
+        GObject parent;
+        
+	LoquiMemberPowerType power;
+	LoquiUser *user;
+
+        LoquiMemberPrivate *priv;
+};
+
+struct _LoquiMemberClass
+{
+        GObjectClass parent_class;
+};
+
+
+GType loqui_member_power_type_get_type(void) G_GNUC_CONST;
+GType loqui_member_get_type(void) G_GNUC_CONST;
+
+LoquiMember* loqui_member_new(void);
+
+void loqui_member_set_power(LoquiMember *member, LoquiMemberPowerType power);
+LoquiMemberPowerType loqui_member_get_power(LoquiMember *member);
+
+void loqui_member_set_user(LoquiMember *member, LoquiUser *user);
+LoquiUser* loqui_member_get_user(LoquiMember *member);
+
+G_END_DECLS
+
+#endif /* __LOQUI_MEMBER_H__ */
