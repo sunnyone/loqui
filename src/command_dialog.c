a8b97ed923e104cde844e293d7f0882bad63627d
sunnyone41 <>
2004-04-09 06:47:31 +0000

    Removed ChannelInputDialog.

--- src/command_dialog.c
+++ src/command_dialog.c
@@ -24,17 +24,11 @@
 #include "intl.h"
 #include <string.h>
 
-
-static void command_dialog_private_talk_cb(Account *account, const gchar *channel_text, const gchar *text, gpointer data);
-static void command_dialog_join_cb(Account *account, const gchar *channel_text, const gchar *text, gpointer data);
-static void command_dialog_part_cb(Account *account, const gchar *channel_text, const gchar *text, gpointer data);
-static void command_dialog_topic_cb(Account *account, const gchar *channel_text, const gchar *text, gpointer data);
-static void command_dialog_nick_cb(Account *account, const gchar *channel_text, const gchar *text, gpointer data);
-
 static gboolean check_account_connected(Account *account);
 static gboolean check_target_valid(const gchar *str);
 
-static gboolean check_account_connected(Account *account)
+static gboolean
+check_account_connected(Account *account)
 {
 	if(account == NULL) {
 		gtkutils_msgbox_info(GTK_MESSAGE_ERROR, _("Account is not selected."));
@@ -46,7 +40,23 @@ static gboolean check_account_connected(Account *account)
 	}
 	return TRUE;
 }
-static gboolean check_target_valid(const gchar *str)
+static gboolean
+check_channel_joined(LoquiChannel *channel)
+{
+	if (channel == NULL) {
+		gtkutils_msgbox_info(GTK_MESSAGE_ERROR, _("Channel is not selected"));
+		return FALSE;
+	}
+
+	if (!loqui_channel_get_is_joined(channel)) {
+		gtkutils_msgbox_info(GTK_MESSAGE_ERROR, _("You are not joined in this channel."));
+		return FALSE;
+	}
+	return TRUE;
+}
+
+static gboolean
+check_target_valid(const gchar *str)
 {
 	if(str == NULL || strlen(str) == 0) {
 		gtkutils_msgbox_info(GTK_MESSAGE_ERROR, _("Input some characters."));
@@ -60,93 +70,228 @@ static gboolean check_target_valid(const gchar *str)
 	return TRUE;
 }
 
-static void command_dialog_join_cb(Account *account, const gchar *channel_text, const gchar *text, gpointer data)
+void
+command_dialog_join(LoquiApp *app, Account *account)
 {
-	if(!check_account_connected(account) ||
-	   !check_target_valid(channel_text))
-		return;
-	
-	account_join(account, channel_text, text);
-}
+	GtkWidget *dialog;
+	GtkWidget *label;
+	GtkWidget *entry_name;
+	GtkWidget *entry_key;
+	gchar *buf;
+	const gchar *text_name, *text_key;
+	gint result;
 
-static void command_dialog_private_talk_cb(Account *account, const gchar *channel_text, const gchar *text, gpointer data)
-{
-	if(!check_account_connected(account) ||
-	   !check_target_valid(text))
+	if(!check_account_connected(account))
 		return;
 	
-	account_start_private_talk(account, text);
-}
+	dialog = gtk_dialog_new_with_buttons(_("Join a channel"), GTK_WINDOW(app),
+					     GTK_DIALOG_DESTROY_WITH_PARENT | GTK_DIALOG_MODAL,
+					     GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+					     GTK_STOCK_OK, GTK_RESPONSE_OK,
+					     NULL);
+        gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_OK);
 
-static void command_dialog_part_cb(Account *account, const gchar *channel_text, const gchar *text, gpointer data)
-{
-	if(!check_account_connected(account) ||
-	   !check_target_valid(channel_text))
-		return;
-	
-	account_part(account, channel_text, text);
-}
-static void command_dialog_topic_cb(Account *account, const gchar *channel_text, const gchar *text, gpointer data)
-{
-	if(!check_account_connected(account) ||
-	   !check_target_valid(channel_text))
-		return;
-	
-	account_set_topic(account, channel_text, text);
-}
-static void command_dialog_nick_cb(Account *account, const gchar *channel_text, const gchar *text, gpointer data)
-{
-	if(!check_account_connected(account) ||
-	   !check_target_valid(text))
-		return;
-	
-	account_change_nick(account, text);
-}
+	buf = g_strdup_printf(_("Join a channel with the account '%s'"), loqui_channel_entry_get_name(LOQUI_CHANNEL_ENTRY(account)));
+	label = gtk_label_new(buf);
+	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox), label, FALSE, FALSE, 0);
+	g_free(buf);
 
-void command_dialog_join(LoquiApp *app, Account *account)
-{
-	channel_input_dialog_open(app, 
-				  _("Join channel"),
-				  _("Type channel name to join (and channel key, if any)."),
-				  CHANNEL_HISTORY_SAVED,
-				  command_dialog_join_cb, NULL,
-				  TRUE, account, TRUE, NULL, TRUE, NULL);
+	gtkutils_add_label_entry(GTK_DIALOG(dialog)->vbox, _("Channel name:"), &entry_name, NULL);
+	gtk_entry_set_activates_default(GTK_ENTRY(entry_name), TRUE);
+
+	gtkutils_add_label_entry(GTK_DIALOG(dialog)->vbox, _("Channel key (if any):"), &entry_key, NULL);
+	gtk_entry_set_activates_default(GTK_ENTRY(entry_key), TRUE);
+
+	gtk_widget_show_all(GTK_DIALOG(dialog)->vbox);
+	result = gtk_dialog_run(GTK_DIALOG(dialog));
+
+	if (result == GTK_RESPONSE_OK) {
+		text_name = gtk_entry_get_text(GTK_ENTRY(entry_name));
+		text_key = gtk_entry_get_text(GTK_ENTRY(entry_key));
+
+		if(!check_target_valid(text_name)) {
+			gtk_widget_destroy(dialog);
+			return;
+		}
+
+		if (strlen(text_key) > 0)
+			account_join(account, text_name, text_key);
+		else
+			account_join(account, text_name, NULL);
+	}
+
+	gtk_widget_destroy(dialog);
 }
-void command_dialog_private_talk(LoquiApp *app, Account *account)
+void
+command_dialog_private_talk(LoquiApp *app, Account *account)
 {
-	channel_input_dialog_open(app, 
-				  _("Start private talk"),
-				  _("Type nick to start private talk."),
-				  CHANNEL_HISTORY_NONE,
-				  command_dialog_private_talk_cb, NULL,
-				  TRUE, account, FALSE, NULL, TRUE, NULL);
+	GtkWidget *dialog;
+	GtkWidget *label;
+	GtkWidget *entry_name;
+	gchar *buf;
+	const gchar *text_name;
+	gint result;
+
+	if(!check_account_connected(account))
+		return;
+	
+	dialog = gtk_dialog_new_with_buttons(_("Start private talk"), GTK_WINDOW(app),
+					     GTK_DIALOG_DESTROY_WITH_PARENT | GTK_DIALOG_MODAL,
+					     GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+					     GTK_STOCK_OK, GTK_RESPONSE_OK,
+					     NULL);
+        gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_OK);
+
+	buf = g_strdup_printf(_("Start private talk with the user in '%s'"), loqui_channel_entry_get_name(LOQUI_CHANNEL_ENTRY(account)));
+	label = gtk_label_new(buf);
+	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox), label, FALSE, FALSE, 0);
+	g_free(buf);
+
+	gtkutils_add_label_entry(GTK_DIALOG(dialog)->vbox, _("Nickname:"), &entry_name, NULL);
+	gtk_entry_set_activates_default(GTK_ENTRY(entry_name), TRUE);
+
+	gtk_widget_show_all(GTK_DIALOG(dialog)->vbox);
+	result = gtk_dialog_run(GTK_DIALOG(dialog));
+
+	if (result == GTK_RESPONSE_OK) {
+		text_name = gtk_entry_get_text(GTK_ENTRY(entry_name));
+
+		if (!check_target_valid(text_name)) {
+			gtk_widget_destroy(dialog);
+			return;
+		}
+		account_start_private_talk(account, text_name);
+	}
+	gtk_widget_destroy(dialog);
 }
-void command_dialog_part(LoquiApp *app, Account *account, LoquiChannel *channel)
+void
+command_dialog_part(LoquiApp *app, Account *account, LoquiChannel *channel)
 {
-	channel_input_dialog_open(app, 
-				  _("Part channel"),
-				  _("Type channel name to part and the part message."),
-				  CHANNEL_HISTORY_JOINED,
-				  command_dialog_part_cb, NULL,
-				  TRUE, account, TRUE, channel ? loqui_channel_entry_get_name(LOQUI_CHANNEL_ENTRY(channel)) : NULL, TRUE, NULL);
+	GtkWidget *dialog;
+	GtkWidget *label;
+	GtkWidget *entry;
+	gchar *buf;
+	const gchar *text;
+	gint result;
+
+	if (!check_account_connected(account))
+		return;
+	if (!check_channel_joined(channel))
+		return;
+
+	dialog = gtk_dialog_new_with_buttons(_("Part the channel"), GTK_WINDOW(app),
+					     GTK_DIALOG_DESTROY_WITH_PARENT | GTK_DIALOG_MODAL,
+					     GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+					     GTK_STOCK_OK, GTK_RESPONSE_OK,
+					     NULL);
+        gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_OK);
+
+	buf = g_strdup_printf(_("Part the channel '%s' (in '%s')"),
+			      loqui_channel_entry_get_name(LOQUI_CHANNEL_ENTRY(channel)),
+			      loqui_channel_entry_get_name(LOQUI_CHANNEL_ENTRY(account)));
+
+	label = gtk_label_new(buf);
+	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox), label, FALSE, FALSE, 0);
+	g_free(buf);
+
+	gtkutils_add_label_entry(GTK_DIALOG(dialog)->vbox, _("Part message:"), &entry, NULL);
+	gtk_entry_set_activates_default(GTK_ENTRY(entry), TRUE);
+
+	gtk_widget_show_all(GTK_DIALOG(dialog)->vbox);
+	result = gtk_dialog_run(GTK_DIALOG(dialog));
+
+	if (result == GTK_RESPONSE_OK) {
+		text = gtk_entry_get_text(GTK_ENTRY(entry));
+
+		account_part(account, loqui_channel_entry_get_name(LOQUI_CHANNEL_ENTRY(channel)), text);
+	}
+	gtk_widget_destroy(dialog);
 }
-void command_dialog_topic(LoquiApp *app, Account *account, LoquiChannel *channel)
+void
+command_dialog_topic(LoquiApp *app, Account *account, LoquiChannel *channel)
 {
-	channel_input_dialog_open(app, 
-				  _("Set the topic of the channel"),
-				  _("Type channel name and its topic."),
-				  CHANNEL_HISTORY_JOINED,
-				  command_dialog_topic_cb, NULL,
-				  TRUE, account,
-				  TRUE, channel ? loqui_channel_entry_get_name(LOQUI_CHANNEL_ENTRY(channel)) : NULL,
-				  TRUE, channel ? loqui_channel_entry_get_topic(LOQUI_CHANNEL_ENTRY(channel)) : NULL);
+	GtkWidget *dialog;
+	GtkWidget *label;
+	GtkWidget *entry;
+	gchar *buf;
+	const gchar *text;
+	gint result;
+
+	if (!check_account_connected(account))
+		return;
+	if (!check_channel_joined(channel))
+		return;
+
+	dialog = gtk_dialog_new_with_buttons(_("Set topic"), GTK_WINDOW(app),
+					     GTK_DIALOG_DESTROY_WITH_PARENT | GTK_DIALOG_MODAL,
+					     GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+					     GTK_STOCK_OK, GTK_RESPONSE_OK,
+					     NULL);
+        gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_OK);
+
+
+	buf = g_strdup_printf(_("Set the topic of the channel '%s' (in '%s')"),
+			      loqui_channel_entry_get_name(LOQUI_CHANNEL_ENTRY(channel)),
+			      loqui_channel_entry_get_name(LOQUI_CHANNEL_ENTRY(account)));
+
+	label = gtk_label_new(buf);
+	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox), label, FALSE, FALSE, 0);
+	g_free(buf);
+
+	gtkutils_add_label_entry(GTK_DIALOG(dialog)->vbox, _("Topic:"), &entry, loqui_channel_entry_get_topic(LOQUI_CHANNEL_ENTRY(channel)));
+	gtk_entry_set_activates_default(GTK_ENTRY(entry), TRUE);
+
+	gtk_widget_show_all(GTK_DIALOG(dialog)->vbox);
+	result = gtk_dialog_run(GTK_DIALOG(dialog));
+
+	if (result == GTK_RESPONSE_OK) {
+		text = gtk_entry_get_text(GTK_ENTRY(entry));
+
+		account_set_topic(account, loqui_channel_entry_get_name(LOQUI_CHANNEL_ENTRY(channel)), text);
+	}
+	gtk_widget_destroy(dialog);
 }
-void command_dialog_nick(LoquiApp *app, Account *account)
+void
+command_dialog_nick(LoquiApp *app, Account *account)
 {
-	channel_input_dialog_open(app, 
-				  _("Change nickname"),
-				  _("Type nickname."),
-				  CHANNEL_HISTORY_NONE,
-				  command_dialog_nick_cb, NULL,
-				  TRUE, account, FALSE, NULL, TRUE, loqui_user_get_nick(account_get_user_self(account)));
+	GtkWidget *dialog;
+	GtkWidget *label;
+	GtkWidget *entry;
+	gchar *buf;
+	const gchar *text;
+	gint result;
+
+	if (!check_account_connected(account))
+		return;
+
+	dialog = gtk_dialog_new_with_buttons(_("Change nickname"), GTK_WINDOW(app),
+					     GTK_DIALOG_DESTROY_WITH_PARENT | GTK_DIALOG_MODAL,
+					     GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+					     GTK_STOCK_OK, GTK_RESPONSE_OK,
+					     NULL);
+        gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_OK);
+
+
+	buf = g_strdup_printf(_("New nickname of the account '%s'"), loqui_channel_entry_get_name(LOQUI_CHANNEL_ENTRY(account)));
+	label = gtk_label_new(buf);
+	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox), label, FALSE, FALSE, 0);
+	g_free(buf);
+
+	gtkutils_add_label_entry(GTK_DIALOG(dialog)->vbox, _("Nickname:"), &entry, loqui_user_get_nick(account_get_user_self(account)));
+	gtk_entry_set_activates_default(GTK_ENTRY(entry), TRUE);
+
+	gtk_widget_show_all(GTK_DIALOG(dialog)->vbox);
+	result = gtk_dialog_run(GTK_DIALOG(dialog));
+
+	if (result == GTK_RESPONSE_OK) {
+		text = gtk_entry_get_text(GTK_ENTRY(entry));
+
+		if(!check_target_valid(text)) {
+			gtk_widget_destroy(dialog);
+			return;
+		}
+
+		account_change_nick(account, text);
+	}
+	gtk_widget_destroy(dialog);
 }
