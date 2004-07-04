68476993d1c32213814ab6682c158789433e9d13
sunnyone41 <>
2004-07-04 08:12:28 +0000

    added LoquiProtocol

---
+++ src/loqui_protocol.c
@@ -0,0 +1,234 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Loqui -- Chat client for Gtk2 <http://loqui.good-day.net/>
+ * Copyright (C) 2004 Yoichi Imai <yoichi@silver-forest.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+#include "config.h"
+
+#include "loqui_protocol.h"
+
+enum {
+        LAST_SIGNAL
+};
+
+enum {
+        LAST_PROP
+};
+
+struct _LoquiProtocolPrivate
+{
+};
+
+static GObjectClass *parent_class = NULL;
+
+/* static guint loqui_protocol_signals[LAST_SIGNAL] = { 0 }; */
+
+static void loqui_protocol_class_init(LoquiProtocolClass *klass);
+static void loqui_protocol_init(LoquiProtocol *protocol);
+static void loqui_protocol_finalize(GObject *object);
+static void loqui_protocol_dispose(GObject *object);
+
+static void loqui_protocol_get_property(GObject *object, guint param_id, GValue *value, GParamSpec *pspec);
+static void loqui_protocol_set_property(GObject *object, guint param_id, const GValue *value, GParamSpec *pspec);
+
+GType
+loqui_protocol_get_type(void)
+{
+	static GType type = 0;
+	if (type == 0) {
+		static const GTypeInfo our_info =
+			{
+				sizeof(LoquiProtocolClass),
+				NULL,           /* base_init */
+				NULL,           /* base_finalize */
+				(GClassInitFunc) loqui_protocol_class_init,
+				NULL,           /* class_finalize */
+				NULL,           /* class_data */
+				sizeof(LoquiProtocol),
+				0,              /* n_preallocs */
+				(GInstanceInitFunc) loqui_protocol_init
+			};
+		
+		type = g_type_register_static(G_TYPE_OBJECT,
+					      "LoquiProtocol",
+					      &our_info,
+					      0);
+	}
+	
+	return type;
+}
+static void 
+loqui_protocol_finalize(GObject *object)
+{
+	LoquiProtocol *protocol;
+
+        g_return_if_fail(object != NULL);
+        g_return_if_fail(LOQUI_IS_PROTOCOL(object));
+
+        protocol = LOQUI_PROTOCOL(object);
+
+        if (G_OBJECT_CLASS(parent_class)->finalize)
+                (* G_OBJECT_CLASS(parent_class)->finalize)(object);
+
+	g_free(protocol->priv);
+}
+static void 
+loqui_protocol_dispose(GObject *object)
+{
+	LoquiProtocol *protocol;
+
+        g_return_if_fail(object != NULL);
+        g_return_if_fail(LOQUI_IS_PROTOCOL(object));
+
+        protocol = LOQUI_PROTOCOL(object);
+
+	G_FREE_UNLESS_NULL(protocol->identifier);
+
+        if (G_OBJECT_CLASS(parent_class)->dispose)
+                (* G_OBJECT_CLASS(parent_class)->dispose)(object);
+}
+static void
+loqui_protocol_get_property(GObject *object, guint param_id, GValue *value, GParamSpec *pspec)
+{
+        LoquiProtocol *protocol;        
+
+        protocol = LOQUI_PROTOCOL(object);
+
+        switch (param_id) {
+        default:
+                G_OBJECT_WARN_INVALID_PROPERTY_ID(object, param_id, pspec);
+                break;
+        }
+}
+static void
+loqui_protocol_set_property(GObject *object, guint param_id, const GValue *value, GParamSpec *pspec)
+{
+        LoquiProtocol *protocol;        
+
+        protocol = LOQUI_PROTOCOL(object);
+
+        switch (param_id) {
+        default:
+                G_OBJECT_WARN_INVALID_PROPERTY_ID(object, param_id, pspec);
+                break;
+        }
+}
+
+static void
+loqui_protocol_class_init(LoquiProtocolClass *klass)
+{
+        GObjectClass *object_class = G_OBJECT_CLASS(klass);
+
+        parent_class = g_type_class_peek_parent(klass);
+        
+        object_class->finalize = loqui_protocol_finalize;
+        object_class->dispose = loqui_protocol_dispose;
+        object_class->get_property = loqui_protocol_get_property;
+        object_class->set_property = loqui_protocol_set_property;
+}
+static void 
+loqui_protocol_init(LoquiProtocol *protocol)
+{
+	LoquiProtocolPrivate *priv;
+
+	priv = g_new0(LoquiProtocolPrivate, 1);
+
+	protocol->priv = priv;
+
+	protocol->type_account = LOQUI_TYPE_ACCOUNT;
+	protocol->type_channel = LOQUI_TYPE_CHANNEL;
+	protocol->type_sender = LOQUI_TYPE_USER;
+	protocol->type_receiver = LOQUI_TYPE_RECEIVER;
+	protocol->type_user = LOQUI_TYPE_USER;
+	protocol->type_profile_account = LOQUI_TYPE_PROFILE_ACCOUNT;
+}
+LoquiProtocol*
+loqui_protocol_new(void)
+{
+        LoquiProtocol *protocol;
+	LoquiProtocolPrivate *priv;
+
+	protocol = g_object_new(loqui_protocol_get_type(), NULL);
+	
+        priv = protocol->priv;
+
+        return protocol;
+}
+void
+loqui_protocol_set_identifier(LoquiProtocol *protocol, const gchar *identifier)
+{
+	G_FREE_UNLESS_NULL(protocol->identifier);
+	if (identifier)
+		identifier = g_strdup(identifier);
+}
+G_CONST_RETURN gchar *
+loqui_protocol_get_identifier(LoquiProtocol *protocol)
+{
+	g_return_val_if_fail(protocol != NULL, NULL);
+	g_return_val_if_fail(LOQUI_IS_PROTOCOL(protocol), NULL);
+
+	return protocol->identifier;
+}
+
+LoquiAccountClass *
+loqui_protocol_get_account_class(LoquiProtocol *protocol)
+{
+	g_return_val_if_fail(protocol != NULL, NULL);
+	g_return_val_if_fail(LOQUI_IS_PROTOCOL(protocol), NULL);
+
+	return g_type_class_ref(protocol->type_account);
+}
+LoquiChannelClass *
+loqui_protocol_get_channel_class(LoquiProtocol *protocol)
+{
+	g_return_val_if_fail(protocol != NULL, NULL);
+	g_return_val_if_fail(LOQUI_IS_PROTOCOL(protocol), NULL);
+
+	return g_type_class_ref(protocol->type_channel);
+}
+LoquiSenderClass *
+loqui_protocol_get_sender_class(LoquiProtocol *protocol)
+{
+	g_return_val_if_fail(protocol != NULL, NULL);
+	g_return_val_if_fail(LOQUI_IS_PROTOCOL(protocol), NULL);
+
+	return g_type_class_ref(protocol->type_sender);
+}
+LoquiReceiverClass *
+loqui_protocol_get_receiver_class(LoquiProtocol *protocol)
+{
+	g_return_val_if_fail(protocol != NULL, NULL);
+	g_return_val_if_fail(LOQUI_IS_PROTOCOL(protocol), NULL);
+
+	return g_type_class_ref(protocol->type_receiver);
+}
+LoquiUserClass *
+loqui_protocol_get_user_class(LoquiProtocol *protocol)
+{
+	g_return_val_if_fail(protocol != NULL, NULL);
+	g_return_val_if_fail(LOQUI_IS_PROTOCOL(protocol), NULL);
+
+	return g_type_class_ref(protocol->type_user);
+}
+LoquiProfileAccountClass *
+loqui_protocol_get_profile_account_class(LoquiProtocol *protocol)
+{
+	g_return_val_if_fail(protocol != NULL, NULL);
+	g_return_val_if_fail(LOQUI_IS_PROTOCOL(protocol), NULL);
+
+	return g_type_class_ref(protocol->type_profile_account);
+}
---
+++ src/loqui_protocol.h
@@ -0,0 +1,87 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Loqui -- Chat client for Gtk2 <http://loqui.good-day.net/>
+ * Copyright (C) 2004 Yoichi Imai <yoichi@silver-forest.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+/* Abstract Factory (Factory Method, Singleton(?)) */
+
+#ifndef __LOQUI_PROTOCOL_H__
+#define __LOQUI_PROTOCOL_H__
+
+#include <glib-object.h>
+#include "gobject_utils.h"
+
+G_BEGIN_DECLS
+
+#define LOQUI_TYPE_PROTOCOL                 (loqui_protocol_get_type ())
+#define LOQUI_PROTOCOL(obj)                 (G_TYPE_CHECK_INSTANCE_CAST ((obj), LOQUI_TYPE_PROTOCOL, LoquiProtocol))
+#define LOQUI_PROTOCOL_CLASS(klass)         (G_TYPE_CHECK_CLASS_CAST ((klass), LOQUI_TYPE_PROTOCOL, LoquiProtocolClass))
+#define LOQUI_IS_PROTOCOL(obj)              (G_TYPE_CHECK_INSTANCE_TYPE ((obj), LOQUI_TYPE_PROTOCOL))
+#define LOQUI_IS_PROTOCOL_CLASS(klass)      (G_TYPE_CHECK_CLASS_TYPE ((klass), LOQUI_TYPE_PROTOCOL))
+#define LOQUI_PROTOCOL_GET_CLASS(obj)       (G_TYPE_INSTANCE_GET_CLASS ((obj), LOQUI_TYPE_PROTOCOL, LoquiProtocolClass))
+
+typedef struct _LoquiProtocol            LoquiProtocol;
+typedef struct _LoquiProtocolClass       LoquiProtocolClass;
+
+typedef struct _LoquiProtocolPrivate     LoquiProtocolPrivate;
+
+#include "loqui_account.h"
+#include "loqui_channel.h"
+#include "loqui_sender.h"
+#include "loqui_receiver.h"
+#include "loqui_user.h"
+#include "loqui_profile_account.h"
+
+struct _LoquiProtocol
+{
+        GObject parent;
+        
+	gchar *identifier;
+
+	GType type_account;
+	GType type_channel;
+	GType type_sender;
+	GType type_receiver;
+	GType type_user;
+	GType type_profile_account;
+	
+        LoquiProtocolPrivate *priv;
+};
+
+struct _LoquiProtocolClass
+{
+        GObjectClass parent_class;
+};
+
+GType loqui_protocol_get_type(void) G_GNUC_CONST;
+
+LoquiProtocol* loqui_protocol_new(void);
+
+void loqui_protocol_set_identifier(LoquiProtocol *protocol, const gchar *identifier);
+G_CONST_RETURN gchar *loqui_protocol_get_identifier(LoquiProtocol *protocol);
+
+/* you must unref classes */
+LoquiAccountClass *loqui_protocol_get_account_class(LoquiProtocol *protocol);
+LoquiChannelClass *loqui_protocol_get_channel_class(LoquiProtocol *protocol);
+LoquiSenderClass *loqui_protocol_get_sender_class(LoquiProtocol *protocol);
+LoquiReceiverClass *loqui_protocol_get_receiver_class(LoquiProtocol *protocol);
+LoquiUserClass *loqui_protocol_get_user_class(LoquiProtocol *protocol);
+LoquiProfileAccountClass *loqui_protocol_get_profile_account_class(LoquiProtocol *protocol);
+
+G_END_DECLS
+
+#endif /* __LOQUI_PROTOCOL_H__ */
