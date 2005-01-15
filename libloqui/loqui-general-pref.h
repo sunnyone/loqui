e24d5b2af9c01fa5c279fbf3cf8ccfe45d253ddf
sunnyone41 <>
2005-01-15 07:05:56 +0000

    Added general_pref

---
+++ libloqui/loqui-general-pref.h
@@ -0,0 +1,18 @@
+#ifndef __LOQUI_GENERAL_PREF__
+#define __LOQUI_GENERAL_PREF__
+
+#include <loqui.h>
+#include <loqui-core.h>
+#include <loqui-pref.h>
+
+#define LOQUI_GET_GENERAL_PREF() loqui_core_get_general_pref(loqui_get_core())
+
+#define LOQUI_GENERAL_PREF_SET_STRING(group, key, val) loqui_pref_set_string(LOQUI_GET_GENERAL_PREF(), group, key, val)
+#define LOQUI_GENERAL_PREF_GET_STRING(group, key) loqui_pref_get_string(LOQUI_GET_GENERAL_PREF(), group, key, NULL)
+#define LOQUI_GENERAL_PREF_SET_STRING_DEFAULT(group, key, val) loqui_pref_set_string_default(LOQUI_GET_GENERAL_PREF(), group, key, val)
+
+#define LOQUI_GENERAL_PREF_SET_BOOLEAN(group, key, val) loqui_pref_set_boolean(LOQUI_GET_GENERAL_PREF(), group, key, val)
+#define LOQUI_GENERAL_PREF_GET_BOOLEAN(group, key) loqui_pref_get_boolean(LOQUI_GET_GENERAL_PREF(), group, key, NULL)
+#define LOQUI_GENERAL_PREF_SET_BOOLEAN_DEFAULT(group, key, val) loqui_pref_set_boolean_default(LOQUI_GET_GENERAL_PREF(), group, key, val)
+
+#endif /* __LOQUI_GENERAL_PREF__ */
