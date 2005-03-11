ffcb4e2afc1a448fd422eb19df2a300f41af6ade
sunnyone41 <>
2005-03-11 09:49:19 +0000

    Added shadow to scrolled windows of tree views

--- src/loqui_app.c
+++ src/loqui_app.c
@@ -664,6 +664,7 @@ loqui_app_new(LoquiAccountManager *account_manager)
 { \
 	s = gtk_scrolled_window_new(NULL, NULL); \
         gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(s), vpolicy, hpolicy); \
+        gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(s), GTK_SHADOW_IN); \
 	gtk_container_add(GTK_CONTAINER(s), w); \
 }
 
