abbf936bae5a731e34b146e2997362ec6ff1fb43
sunnyone41 <>
2005-05-15 08:09:42 +0000

    Fetch the list of files from svn

--- update-potfiles.sh
+++ update-potfiles.sh
@@ -1,1 +1,5 @@
-ls -1 libloqui/*.[ch] src/*.[ch] > po/POTFILES.in
+#!/bin/sh
+rm po/POTFILES.in
+for I in libloqui src; do
+  svn ls $I | egrep '\.(c|h|gob)$' | sed -e "s/^/$I\//" >> po/POTFILES.in
+done
