48b0987aff51000a86a695657632a7a8aa6f26c2
sunnyone41 <>
2006-01-17 08:09:00 +0000

    Check directories recursively.

--- update-potfiles.sh
+++ update-potfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm po/POTFILES.in
-for I in libloqui src; do
-  svn ls $I | egrep '\.(c|h|gob)$' | sed -e "s/^/$I\//" >> po/POTFILES.in
+for I in libloqui src `find libloqui/protocols -type d | egrep -v "/\\."`; do
+  svn ls $I | egrep '\.(c|h|gob)$' | sed -e "s,^,$I\/," >> po/POTFILES.in
 done
