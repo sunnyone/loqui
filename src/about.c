43e138ceb336857269c34724f49adfb8d9807233
Yoichi Imai <sunnyone41@gmail.com>
2015-12-13 02:18:25 +0900

    Extend copyright year.

--- src/about.c
+++ src/about.c
@@ -29,6 +29,6 @@ about_open(void)
 	gtkutils_msgbox_info(GTK_MESSAGE_INFO,
 			     "%s\n"
 			     "IRC Client for Gtk2\n"
-			     "Copyright (C) 2002-2013, Yoichi Imai <sunnyone41@gmail.com>\n"
+			     "Copyright (C) 2002-2015, Yoichi Imai <sunnyone41@gmail.com>\n"
 			     "http://launchpad.net/loqui/", loqui_core_get_version_info(loqui_get_core()));
 }
